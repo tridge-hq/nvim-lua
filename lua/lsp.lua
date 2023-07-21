e3d8eb3109f990042ec3246c0f2483fd222931b2
Jinsu Kim <jinsu.kim@tridge.com>
2023-07-21 16:44:58 +0900

    Modify goto deifinition and remove unused plugins

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -18,8 +18,29 @@ require('mason-lspconfig').setup {
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'gD', vim.bo.filetype == 'python' and vim.lsp.buf.declaration or vim.lsp.buf.definition, opts)
+  -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
+end
 
-require('lspconfig')['pyright'].setup {
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local lspconfig = require('lspconfig')
+lspconfig['pyright'].setup ({
+  on_attach = on_attach,
+  capabilities = capabilities,
   settings = {
     python = {
       analysis = {
@@ -28,9 +49,11 @@ require('lspconfig')['pyright'].setup {
       },
     },
   },
-}
+})
 
-require('lspconfig')['lua_ls'].setup {
+lspconfig['lua_ls'].setup ({
+  on_attach = on_attach,
+  capabilities = capabilities,
   settings = {
     Lua = {
       diagnostics = {
@@ -40,7 +63,7 @@ require('lspconfig')['lua_ls'].setup {
       },
     },
   }
-}
+})
 
 -- Ref: https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
 for _, value in ipairs({
@@ -49,7 +72,10 @@ for _, value in ipairs({
   'tsserver',
   'yamlls',
 }) do
-  require('lspconfig')[value].setup {}
+  lspconfig[value].setup ({
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
 end
 
 
@@ -91,13 +117,4 @@ cmp.setup.cmdline(':', {
   }, {
     { name = 'cmdline' }
   })
-})
-
-require('lspsaga').setup({
-  lightbulb = {
-    enable = false,
-  },
-  symbol_in_winbar = {
-    enable = false,
-  }
 })
